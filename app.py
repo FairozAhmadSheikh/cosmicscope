ec8390cb19eb6989e6974f29fefebe750306fe11
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-10-17 21:25:01 +0530

    secret key

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ from utils.db import get_db
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+MONGO_URI = os.environ.get('MONGO_URI')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'replace-with-secure-key')
+
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+app.secret_key = SECRET_KEY
